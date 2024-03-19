e76581fe24bb3973ab24c6e70f648f7645aaa458
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-03-19 16:46:09 +0530

    add firebase configuration

--- frontend_project/src/App.jsx
+++ frontend_project/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const App = () => {
   return (
     <>
-      
+      <h1>Welcome to chat application project</h1>
     </>
   )
 }
---
+++ frontend_project/src/firebase_config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC101Irra30Br0TOXUMkdXU3Spn-mWcqz8",
+    authDomain: "realtime-chat-applicatio-bc9f9.firebaseapp.com",
+    projectId: "realtime-chat-applicatio-bc9f9",
+    storageBucket: "realtime-chat-applicatio-bc9f9.appspot.com",
+    messagingSenderId: "898162762615",
+    appId: "1:898162762615:web:38409abfcb27ca6431b772"
+  };
